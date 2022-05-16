7a3f966bb8b7c5566816f5a8566f77c81e6b99ff
Pedro Caetano <pedrocaetano90@gmail.com>
2022-05-16 12:51:43 +0200

    Added very dim background to navbar Added some TODOs

--- containers/navbar/navbar.container.tsx
+++ containers/navbar/navbar.container.tsx
@@ -2,6 +2,8 @@ import { MenuHamburger } from '../../components/menu-hamburger/menu-hamburger.co
 import { Footer } from '../footer/footer.component';
 import style from './navbar.module.scss';
 
+// TODO: A lot of empty space in big screens below menu (make bigger fonts and a bit more bottom padding)
+// TODO: Finish mobile version
 export function Navbar() {
     return (
         <div className={style.container}>
